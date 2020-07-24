6d255f7ef6e793cf38baf3af501a227ead9c4253
Afanasjew-Aleksey <61011916+Afanasjew-Aleksey@users.noreply.github.com>
2020-07-24 10:02:54 +0400

    Add files via upload

---
+++ Project 1.4/Project 1.3/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace Project_1._3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.TextError = new System.Windows.Forms.TextBox();
+            this.TxtDB = new System.Windows.Forms.TextBox();
+            this.Read = new System.Windows.Forms.Button();
+            this.Label6 = new System.Windows.Forms.Label();
+            this.TxtDump = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.DisconnectBtn = new System.Windows.Forms.Button();
+            this.ConnectBtn = new System.Windows.Forms.Button();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.TxtSlot = new System.Windows.Forms.TextBox();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.TxtRack = new System.Windows.Forms.TextBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.TxtIP = new System.Windows.Forms.TextBox();
+            this.TxtList = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(24, 72);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(176, 24);
+            this.comboBox1.TabIndex = 78;
+            this.comboBox1.Text = "OB";
+            // 
+            // TextError
+            // 
+            this.TextError.BackColor = System.Drawing.Color.White;
+            this.TextError.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.TextError.ForeColor = System.Drawing.Color.Black;
+            this.TextError.Location = new System.Drawing.Point(0, 428);
+            this.TextError.Margin = new System.Windows.Forms.Padding(4);
+            this.TextError.Name = "TextError";
+            this.TextError.ReadOnly = true;
+            this.TextError.Size = new System.Drawing.Size(1336, 22);
+            this.TextError.TabIndex = 77;
+            // 
+            // TxtDB
+            // 
+            this.TxtDB.Enabled = false;
+            this.TxtDB.Location = new System.Drawing.Point(496, 164);
+            this.TxtDB.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtDB.Name = "TxtDB";
+            this.TxtDB.Size = new System.Drawing.Size(59, 22);
+            this.TxtDB.TabIndex = 76;
+            this.TxtDB.Text = "1";
+            // 
+            // Read
+            // 
+            this.Read.Enabled = false;
+            this.Read.Location = new System.Drawing.Point(16, 112);
+            this.Read.Margin = new System.Windows.Forms.Padding(4);
+            this.Read.Name = "Read";
+            this.Read.Size = new System.Drawing.Size(129, 28);
+            this.Read.TabIndex = 71;
+            this.Read.Text = "Read";
+            this.Read.UseVisualStyleBackColor = true;
+            this.Read.Click += new System.EventHandler(this.Read_Click);
+            // 
+            // Label6
+            // 
+            this.Label6.AutoSize = true;
+            this.Label6.Location = new System.Drawing.Point(430, 163);
+            this.Label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Label6.Name = "Label6";
+            this.Label6.Size = new System.Drawing.Size(58, 17);
+            this.Label6.TabIndex = 69;
+            this.Label6.Text = "Number";
+            // 
+            // TxtDump
+            // 
+            this.TxtDump.BackColor = System.Drawing.Color.White;
+            this.TxtDump.Enabled = false;
+            this.TxtDump.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtDump.ForeColor = System.Drawing.Color.Black;
+            this.TxtDump.Location = new System.Drawing.Point(576, 83);
+            this.TxtDump.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtDump.Multiline = true;
+            this.TxtDump.Name = "TxtDump";
+            this.TxtDump.ReadOnly = true;
+            this.TxtDump.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TxtDump.Size = new System.Drawing.Size(724, 337);
+            this.TxtDump.TabIndex = 68;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(43, 382);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 17);
+            this.label4.TabIndex = 64;
+            this.label4.Text = "label4";
+            // 
+            // DisconnectBtn
+            // 
+            this.DisconnectBtn.Enabled = false;
+            this.DisconnectBtn.Location = new System.Drawing.Point(316, 40);
+            this.DisconnectBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.DisconnectBtn.Name = "DisconnectBtn";
+            this.DisconnectBtn.Size = new System.Drawing.Size(133, 28);
+            this.DisconnectBtn.TabIndex = 63;
+            this.DisconnectBtn.Text = "Disconnect";
+            this.DisconnectBtn.UseVisualStyleBackColor = true;
+            this.DisconnectBtn.Click += new System.EventHandler(this.DisconnectBtn_Click_1);
+            // 
+            // ConnectBtn
+            // 
+            this.ConnectBtn.Location = new System.Drawing.Point(316, 8);
+            this.ConnectBtn.Margin = new System.Windows.Forms.Padding(4);
+            this.ConnectBtn.Name = "ConnectBtn";
+            this.ConnectBtn.Size = new System.Drawing.Size(133, 28);
+            this.ConnectBtn.TabIndex = 60;
+            this.ConnectBtn.Text = "Connect";
+            this.ConnectBtn.UseVisualStyleBackColor = true;
+            this.ConnectBtn.Click += new System.EventHandler(this.ConnectBtn_Click);
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(234, 5);
+            this.Label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(32, 17);
+            this.Label3.TabIndex = 62;
+            this.Label3.Text = "Slot";
+            // 
+            // TxtSlot
+            // 
+            this.TxtSlot.Location = new System.Drawing.Point(234, 27);
+            this.TxtSlot.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtSlot.Name = "TxtSlot";
+            this.TxtSlot.Size = new System.Drawing.Size(57, 22);
+            this.TxtSlot.TabIndex = 59;
+            this.TxtSlot.Text = "2";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(160, 5);
+            this.Label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(40, 17);
+            this.Label2.TabIndex = 61;
+            this.Label2.Text = "Rack";
+            // 
+            // TxtRack
+            // 
+            this.TxtRack.Location = new System.Drawing.Point(160, 27);
+            this.TxtRack.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtRack.Name = "TxtRack";
+            this.TxtRack.Size = new System.Drawing.Size(57, 22);
+            this.TxtRack.TabIndex = 57;
+            this.TxtRack.Text = "0";
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(13, 5);
+            this.Label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(76, 17);
+            this.Label1.TabIndex = 58;
+            this.Label1.Text = "IP Address";
+            // 
+            // TxtIP
+            // 
+            this.TxtIP.Location = new System.Drawing.Point(13, 27);
+            this.TxtIP.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtIP.Name = "TxtIP";
+            this.TxtIP.Size = new System.Drawing.Size(132, 22);
+            this.TxtIP.TabIndex = 56;
+            this.TxtIP.Text = "62.213.11.154";
+            // 
+            // TxtList
+            // 
+            this.TxtList.BackColor = System.Drawing.Color.White;
+            this.TxtList.Enabled = false;
+            this.TxtList.Font = new System.Drawing.Font("Courier New", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TxtList.ForeColor = System.Drawing.Color.Black;
+            this.TxtList.Location = new System.Drawing.Point(16, 159);
+            this.TxtList.Margin = new System.Windows.Forms.Padding(4);
+            this.TxtList.Multiline = true;
+            this.TxtList.Name = "TxtList";
+            this.TxtList.ReadOnly = true;
+            this.TxtList.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.TxtList.Size = new System.Drawing.Size(236, 219);
+            this.TxtList.TabIndex = 79;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(163, 112);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 80;
+            this.button1.Text = "List";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(316, 112);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 81;
+            this.button2.Text = "Save";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Visible = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(422, 112);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 82;
+            this.button3.Text = "Load";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(316, 163);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 83;
+            this.button4.Text = "Delete";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Visible = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1336, 450);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TxtList);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.TextError);
+            this.Controls.Add(this.TxtDB);
+            this.Controls.Add(this.Read);
+            this.Controls.Add(this.Label6);
+            this.Controls.Add(this.TxtDump);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.DisconnectBtn);
+            this.Controls.Add(this.ConnectBtn);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.TxtSlot);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.TxtRack);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.TxtIP);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        internal System.Windows.Forms.TextBox TextError;
+        internal System.Windows.Forms.TextBox TxtDB;
+        internal System.Windows.Forms.Button Read;
+        internal System.Windows.Forms.Label Label6;
+        internal System.Windows.Forms.TextBox TxtDump;
+        private System.Windows.Forms.Label label4;
+        internal System.Windows.Forms.Button DisconnectBtn;
+        internal System.Windows.Forms.Button ConnectBtn;
+        internal System.Windows.Forms.Label Label3;
+        internal System.Windows.Forms.TextBox TxtSlot;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.TextBox TxtRack;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.TextBox TxtIP;
+        internal System.Windows.Forms.TextBox TxtList;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Project 1.4/Project 1.3/Form1.cs
@@ -0,0 +1,323 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_1._3
+{
+    public partial class Form1 : Form
+    {
+        static Snap7.S7Client Client = new Snap7.S7Client();
+        byte[] Buffer = new byte[65536];
+        ushort[] List = new ushort[1000];
+        int[] Block_Of = new int[7] { Client.Block_ob(), Client.Block_fb(), Client.Block_fc(), Client.Block_db(),
+            Client.Block_sfb(), Client.Block_sfc(), Client.Block_sdb() };
+        String Adress = "";
+        public Form1()
+        {
+            InitializeComponent();
+            comboBox1.Items.AddRange(new object[] {
+            "OB", "FB", "FC", "DB", "SFB", "SFC", "SDB"});
+        }
+
+        private void ConnectBtn_Click(object sender, EventArgs e)
+        {
+            String IPAdress = TxtIP.Text;
+            int Rack = System.Convert.ToInt32(TxtRack.Text);
+            int Slot = System.Convert.ToInt32(TxtSlot.Text);
+            int Result = Client.ConnectTo(IPAdress, Rack, Slot);
+            ShowResult(Result);
+            if (Result == 0)
+            {
+                TxtIP.Enabled = false;
+                TxtRack.Enabled = false;
+                TxtSlot.Enabled = false;
+                comboBox1.Enabled = true;
+                ConnectBtn.Enabled = false;
+                DisconnectBtn.Enabled = true;
+                Read.Enabled = true;
+                TxtDB.Enabled = true;
+                label4.Text = "Start";
+                button1.Visible = true;
+                button2.Visible = true;
+                button3.Visible = true;
+                button4.Visible = true;
+            }
+            else
+            {
+                label4.Text = "Fail";
+            }
+        }
+
+        private void ShowResult(int Result)
+        {
+            TextError.Text = Client.ErrorText(Result);
+        }
+
+        private void Read_Click(object sender, EventArgs e)
+        {
+            int Result, c;
+            string s;
+
+            int BlockNumber, BlockType, BlockSize;
+
+            BlockType = Block_Of[comboBox1.SelectedIndex];
+            BlockNumber = System.Convert.ToInt32(TxtDB.Text);
+            BlockSize = 65536;
+            // Unsafe context: can use pointers here.
+            label4.Text = BlockType.ToString();
+
+            Result = Client.FullUpload(BlockType, BlockNumber, Buffer, ref BlockSize);
+            ShowResult(Result);
+            if (Result == 0)
+            {
+                //label4.Text = "";
+                TxtDump.Clear();
+                for (c = 0; c <= BlockSize - 1; c++)
+                {
+                    s = Convert.ToString(Buffer[c], 16);
+                    if (s.Length == 1)
+                    {
+                        s = "0" + s;
+                    }
+                    TxtDump.Text = TxtDump.Text + s + " ";
+                    //y = y + 1;
+                    //if (y == 8)
+                    //{
+                    //    y = 0;
+                    //    Text = Text + "\n";
+                    //}
+                }
+            }
+            else
+            {
+                label4.Text = Result.ToString();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string path, folderName = @"\Test";
+
+            int BlockType = Block_Of[comboBox1.SelectedIndex];
+            int BlockSize = 655360, Count = 1000, i;
+            int result = Client.ListBlocksOfType(BlockType, List, ref Count);
+            ShowResult(result);
+            if (result == 0)
+            {
+                TxtList.Clear();
+                for (i = 0; i < Count; i++)
+                {
+                    string s = Convert.ToString(List[i], 10);
+                    TxtList.Text = TxtList.Text + s + " ";
+                }
+            }
+        }
+
+        private void DisconnectBtn_Click_1(object sender, EventArgs e)
+        {
+            Client.Disconnect();
+            TxtIP.Enabled = true;
+            TxtRack.Enabled = true;
+            TxtSlot.Enabled = true;
+            ConnectBtn.Enabled = true;
+            comboBox1.Enabled = false;
+            DisconnectBtn.Enabled = false;
+            Read.Enabled = false;
+            TxtDB.Enabled = false;
+            button1.Visible = false;
+            button2.Visible = false;
+            button3.Visible = false;
+            button4.Visible = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string firstName = @"\Saves";
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.DirectoryInfo drInfoFirst = new DirectoryInfo(fbd.SelectedPath + firstName);
+                if (!drInfoFirst.Exists)
+                {
+                    drInfoFirst.Create();
+                }
+                MessageBox.Show("OK");
+
+            }
+            Adress = fbd.SelectedPath + firstName;
+
+            SaveBlock(@"\OB", Adress, Client.Block_ob());
+            SaveBlock(@"\DB", Adress, Client.Block_db());
+            SaveBlock(@"\SDB", Adress, Client.Block_sdb());
+            SaveBlock(@"\FC", Adress, Client.Block_fc());
+            SaveBlock(@"\FB", Adress, Client.Block_fb());
+        }
+
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int Result, c;
+            string s;
+
+            int BlockNumber, BlockType, BlockSize;
+
+            BlockType = Block_Of[comboBox1.SelectedIndex];
+            BlockNumber = System.Convert.ToInt32(TxtDB.Text);
+            BlockSize = 65536;
+            // Unsafe context: can use pointers here.
+            label4.Text = BlockType.ToString();
+
+            Result = Client.Delete(BlockType, BlockNumber);
+            ShowResult(Result);
+            if (Result == 0)
+            {
+                //label4.Text = "";
+                TxtDump.Clear();
+                for (c = 0; c <= BlockSize - 1; c++)
+                {
+                    s = Convert.ToString(Buffer[c], 16);
+                    if (s.Length == 1)
+                    {
+                        s = "0" + s;
+                    }
+                    TxtDump.Text = "Блок удален";
+                    //y = y + 1;
+                    //if (y == 8)
+                    //{
+                    //    y = 0;
+                    //    Text = Text + "\n";
+                    //}
+                }
+            }
+            else
+            {
+                label4.Text = Result.ToString();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string firstName = @"\Saves";
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.DirectoryInfo drInfoFirst = new DirectoryInfo(fbd.SelectedPath + firstName);
+                if (!drInfoFirst.Exists)
+                {
+                    drInfoFirst.Create();
+                }
+                MessageBox.Show("OK");
+
+            }
+            Adress = fbd.SelectedPath + firstName;
+
+            LoadBlock(@"\OB", Adress, Client.Block_ob());
+            LoadBlock(@"\DB", Adress, Client.Block_db());
+            LoadBlock(@"\SDB", Adress, Client.Block_sdb());
+            LoadBlock(@"\FC", Adress, Client.Block_fc());
+            LoadBlock(@"\FB", Adress, Client.Block_fb());
+
+        }
+
+        private void SaveBlock(string Type_name, string firstname, int type_num)
+        {
+            string path; //firstName = @"\Saves";
+            string number_path;
+
+            StreamWriter output;
+            byte[] Save_Buffer = new byte[65536];
+            ushort[] new_List = new ushort[1000];
+            int Count = 1000, i, number, result2, BlockSize = 65536, result1, inforesult;
+            Snap7.S7Client.S7BlockInfo Info = new Snap7.S7Client.S7BlockInfo();
+
+            System.IO.DirectoryInfo drInfo = new DirectoryInfo(firstname + Type_name);
+            if (!drInfo.Exists)
+            {
+                drInfo.Create();
+            }
+            number_path = firstname + Type_name + @"\Number.txt";
+            output = new StreamWriter(number_path);
+            if (!File.Exists(number_path))
+            {
+                File.Create(number_path);
+            }
+            result1 = Client.ListBlocksOfType(type_num, new_List, ref Count);
+            if (result1 == 0)
+            {
+                TxtDump.Clear();
+                for (i = 0; i < Count; i++)
+                {
+                    number = Convert.ToInt32(new_List[i]);
+                    Save_Buffer = new byte[65536];
+                    inforesult = Client.GetAgBlockInfo(type_num, number, ref Info);
+                    if (inforesult == 0)
+                    {
+                        BlockSize = Info.LoadSize;
+                    }
+                    output.Write(number + ":" + BlockSize + "\n");
+                    Save_Buffer = new byte[BlockSize];
+                    result2 = Client.FullUpload(type_num, number, Save_Buffer, ref BlockSize);
+                    if (result2 == 0)
+                    {
+                        path = firstname + Type_name + @"\states" + number.ToString() + ".dat";
+                        BinaryWriter writer = new BinaryWriter(File.Open(path, FileMode.OpenOrCreate));
+                        writer.Write(Save_Buffer);
+                        writer.Close();
+                    }
+                }
+                output.Close();
+            }
+
+        }
+
+        private void LoadBlock(string Type_name, string firstname, int type_num)
+        {
+            TxtDump.Clear();
+            string path;
+            string number_path, number, line, size;
+
+            StreamReader output;
+            BinaryReader reader_bin;
+            byte[] Save_Buffer;
+            int result2, index;
+
+            number_path = firstname + Type_name + @"\Number.txt";
+            output = new StreamReader(number_path);
+            while ((line = output.ReadLine()) != null)
+            {
+                index = line.LastIndexOf(':');
+                number = line.Substring(0, index);
+
+                size = line.Substring(index+1, line.Length - index-1);
+                path = firstname + Type_name + @"\states" + number + ".dat";
+                //label4.Text = path;
+                reader_bin = new BinaryReader(File.OpenRead(path));
+                Save_Buffer = new byte[Convert.ToInt32(size)];
+                Save_Buffer = reader_bin.ReadBytes(Convert.ToInt32(size));
+                reader_bin.Close();
+                //label4.Text = Int32.Parse(size).ToString();
+                result2 = Client.Download(Convert.ToInt32(number), Save_Buffer, Convert.ToInt32(size));
+                ShowResult(result2);
+                if (result2 == 0)
+                {
+                    //label4.Text += "Gut";
+                }
+                else
+                {
+                    label4.Text +=Type_name +" ";
+                }
+            }
+            output.Close();
+        }
+    }
+}
